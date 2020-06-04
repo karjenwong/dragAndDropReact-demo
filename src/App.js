4fcade2fe221fe10dce4882ca3bfa9ebc4dbf517
karjen <karjenwong@gmail.com>
2020-06-03 17:14:39 -0700

    workable components that drag and drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState([
+    [1, 2],
+    [3, 4, 5, 6],
+  ]);
+  const [dragging, setDragging] = React.useState(false);
+  const dragItem = React.useRef();
+  const itemTarget = React.useRef();
+  const dragNode = React.useRef();
+
+  const onDragStart = (e, params) => {
+    dragItem.current = params;
+    dragNode.current = e.target;
+    dragNode.current.addEventListener("dragend", handleDragEnd);
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+    console.log("start dragging" + dragItem.current);
+  };
+  const handleDragEnd = () => {
+    console.log("drag end");
+    if (itemTarget.current) {
+      let values = [itemTarget.current.pop(), dragItem.current.pop()];
+
+      [dragItem.current, itemTarget.current].forEach(([i, j], index) => {
+        data[i][j] = values[index];
+      });
+
+      setData(data);
+    }
+
+    //clean up
+    setDragging(false);
+    dragNode.current.removeEventListener("dragend", handleDragEnd);
+    dragNode.current = null;
+    dragItem.current = null;
+    itemTarget.current = null;
+  };
+  const handleDragEnter = (e, params) => {
+    if (JSON.stringify(params) !== JSON.stringify(dragItem.current)) {
+      itemTarget.current = params;
+    }
+  };
+  const displayCards = data.map((x, i) => {
+    return (
+      <div className="dnd-group" key={i}>
+        {x.map((y, j) => (
+          <div
+            key={j}
+            className="card dragMove"
+            draggable
+            onDragStart={(e) => onDragStart(e, [i, j, y])}
+            onDragEnter={(e) => handleDragEnter(e, [i, j, y])}
+          >
+            {y}
+          </div>
+        ))}
+      </div>
+    );
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="drag-n-drop">{displayCards}</div>
       </header>
     </div>
   );
